49172ee05ff4b7632fbb167970d46ef59e548c84
Chase Brasfield <chasebrasfield@gmail.com>
2018-10-17 08:26:10 -0500

    Added react router

--- src/components/app.js
+++ src/components/app.js
@@ -1,34 +1,26 @@
 import React from "react";
-
-import peopleList from "./people";
 import FilmsList from "./films";
 import PeopleList from "./people";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 class App extends React.Component {
-  state = {
-    viewingFilms: false,
-    viewingPeople: false
-  };
-
-  loadFilms = () => {
-    this.setState({ viewingFilms: true, viewingPeople: false });
-  };
-
-  loadPeople = () => {
-    this.setState({ viewingFilms: false, viewingPeople: true });
-  };
-
   render() {
     return (
       <div className="container">
-        <button className="btn btn-primary mt-5 mr-5" onClick={this.loadFilms}>
-          Load Films
-        </button>
-        <button className="btn btn-primary mt-5" onClick={this.loadPeople}>
-          Load People
-        </button>
-        {this.state.viewingFilms && <FilmsList />}
-        {this.state.viewingPeople && <PeopleList />}
+          <Router>
+            <div>
+              <Link to='/films' onClick={this.goToFilmsList} className="btn btn-primary mt-5 mr-5">
+                Show Films
+              </Link>
+              <Link to='/people' onClick={this.goToPeopleList} className="btn btn-primary mt-5">
+                Show People
+              </Link>
+              <Switch>
+                <Route path='/films' component={FilmsList} />
+                <Route path='/people' component={PeopleList} />
+              </Switch>
+            </div>
+          </Router>
       </div>
     );
   }
